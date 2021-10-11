7d5add62e0d78cf929abc3805f80bc9fbec18072
Anar Guliev <guliev.anar1998@gmail.com>
2021-10-11 23:02:13 +0300

    Fixed homework

--- src/Components/home-work-2/Form.jsx
+++ src/Components/home-work-2/Form.jsx
@@ -4,17 +4,38 @@ export default function Form() {
   let email = '';
   let password = '';
 
+  const changeValue = (event) => {
+    if (event.target.name === 'Email') {
+      email = event.target.value;
+      console.log(email);
+    } else if (event.target.name === 'Password') {
+      password = event.target.value;
+    }
+  };
+
   const handleSubmit = () => {
     email || password
       ? console.log('Email ' + email + ' Password' + password)
       : alert('Вы не заполнили формы');
+    if (email) {
+      console.log('Email ' + email);
+    } else if (password) {
+      console.log('Password: ' + password);
+    } else {
+      alert('Вы не заполнили форму');
+    }
   };
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" placeholder="E-Mail" onChange={(e) => (email = e.target.value)} />
-        <input type="password" placeholder="Пароль" onChange={(e) => (password = e.target.value)} />
+        <input name="Email" type="text" placeholder="E-Mail" onChange={(e) => changeValue(e)} />
+        <input
+          name="Password"
+          type="password"
+          placeholder="Пароль"
+          onChange={(e) => changeValue(e)}
+        />
         <button type="submit">Войти</button>
       </form>
     </div>
