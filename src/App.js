bb48e1948adf9c161ab16dff26b9841332596e6b
Anar Guliev <guliev.anar1998@gmail.com>
2021-10-16 20:40:44 +0300

    add all

--- src/App.js
+++ src/App.js
@@ -1,91 +1,78 @@
 import React from 'react';
-import List from './Components/home-work-4/List';
-import EmptyBlock from './Components/home-work-4/EmptyBlock';
 import './App.css';
-
-const adjectivesArr = [
-  'абсолютный',
-  'адский',
-  'азартный',
-  'активный',
-  'ангельский',
-  'астрономический',
-  'баснословный',
-  'безбожный',
-  'безбрежный',
-  'безвозвратный',
-  'безграничный',
-  'бездонный',
-  'бездушный',
-  'безжалостный',
-  'замечательно',
-  'замечательный',
-  'записной',
-  'запредельный',
-  'заядлый',
-  'звериный',
-  'зверский',
-  'зеленый',
-  'злой',
-  'злостный',
-  'значительный',
-  'неоспоримый',
-  'неотразимый',
-  'неоценимый',
-  'непередаваемый',
-];
-
-const nounsArr = [
-  'лгун',
-  'день',
-  'конь',
-  'олень',
-  'человек',
-  'программист',
-  'ребёнок',
-  'конец',
-  'город',
-  'дурак',
-];
+import List from './Components/home-work-5/List';
+import { Button, InputLabel } from '@material-ui/core';
 
 function App() {
-  const [words, setWords] = React.useState([]);
+  const [name, setName] = React.useState();
+  const [email, setEmail] = React.useState();
+  const [text, setText] = React.useState();
 
-  const getRandomNumber = (from, before) => {
-    return Math.floor(Math.random() * (before - from) + from);
+  const [data, setData] = React.useState([]);
+  const [comments, setComments] = React.useState([]);
+
+  const changeName = (e) => {
+    setName(e.target.value);
   };
 
-  const getNoun = () => {
-    return nounsArr[getRandomNumber(0, nounsArr.length)];
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
   };
 
-  const getAdjective = () => {
-    return adjectivesArr[getRandomNumber(0, adjectivesArr.length)];
+  const changeText = (e) => {
+    setText(e.target.value);
   };
 
-  const generateWords = () => {
-    return getAdjective() + ' ' + getAdjective() + ' ' + getNoun();
+  const changeData = () => {
+    if (name && email && text) {
+      setData([...data, { fullName: name, email: email, text: text }]);
+      setComments([...comments, { fullName: name, text: text, email: email }]);
+      console.log(comments);
+    } else {
+      alert('Заполните форму!');
+    }
   };
 
-  const getWords = () => {
-    let word = generateWords();
-    setWords([...words, word]);
-  };
+  React.useEffect(() => {
+    if (localStorage.comments) {
+      console.log(localStorage.comments);
+      // Тут не понял как мне в дату запихать данные из локала, потому что это строка. Обьясни пожалуйста никогда не работал с этим.
+      // setData([...data, { fullName: localStorage.comments.name, email: localStorage.comments.email, text: localStorage.comments.text }]);
+    }
+  }, []);
 
-  const deleteWords = () => {
-    setWords([]);
-  };
+  React.useEffect(() => {
+    localStorage.setItem('comments', JSON.stringify(comments));
+  }, [comments]);
 
   return (
     <div className="App">
-      <div className="wrapper">
-        {!words.length ? <EmptyBlock /> : <List words={words} />}
-        <button onClick={getWords} className="btn btn_generate">
-          Сгенерировать
-        </button>
-        <button onClick={deleteWords} className="btn btn_clear">
-          Очистить
-        </button>
+      <div className="textBlock">
+        <h1>Отзывы</h1>
+        {/* Не пойму как написать тут условие, что бы передавать данные либо из даты, либо из localStorage */}
+        {data &&
+          comments &&
+          data.map((item, index) => {
+            return (
+              <List key={index} fullName={item.fullName} email={item.email} text={item.text} />
+            );
+          })}
+      </div>
+
+      <div className="connection">
+        <h2>Обратная связь</h2>
+        <InputLabel shrink htmlFor="bootstrap-input">
+          <input onChange={changeName} type="text" />
+        </InputLabel>
+        <InputLabel shrink htmlFor="bootstrap-input">
+          <input onChange={changeEmail} type="text" />
+        </InputLabel>
+        <InputLabel shrink htmlFor="bootstrap-input">
+          <input id="text" onChange={changeText} type="text" />
+        </InputLabel>
+        <Button onClick={changeData} variant="contained" color="primary">
+          Отправить
+        </Button>
       </div>
     </div>
   );
